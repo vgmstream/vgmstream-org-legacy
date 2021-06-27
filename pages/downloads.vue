8767d7ccbd31e7c3f70adf221c36020f2356b58e
Kevin Doncam Demian López Brante <kevin@kddlb.cl>
2021-06-27 22:15:52 +0000

    Removed Linux downloads

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -13,13 +13,13 @@
       </div> </b-btn>
       <b-btn :href="`https://vgmstream-builds.s3-us-west-1.amazonaws.com/${builds.latestCommitData.sha}/windows/foo_input_vgmstream.fb2k-component`" size="lg" variant="success" class="text-left"><div class="d-inline-flex"><i class="vgs-foobar2000 mr-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>foobar2000 component</div></b-btn>
     </b-button-group>
-    <h2 class="pt-3"><i class="vgs-ubuntu pr-3"></i>Ubuntu 18.04</h2>
+    <!--h2 class="pt-3"><i class="vgs-ubuntu pr-3"></i>Ubuntu 18.04</h2>
     <b-alert show>Install these packages first: <code>sudo apt-get install mpg123 libvorbis0a libvorbisenc2 libvorbisfile3 ffmpeg libao4 libjansson4</code></b-alert>
     <b-button-group vertical>
       <b-btn :href="`https://vgmstream-builds.s3-us-west-1.amazonaws.com/${builds.latestCommitData.sha}/linux/vgmstream-cli.tar.gz`" size="lg" variant="success" class="text-left"><div class="d-inline-flex">
         <i class="vgs-terminal pr-2"></i> Command-line tools</div> </b-btn>
       <b-btn :href="`https://vgmstream-builds.s3-us-west-1.amazonaws.com/${builds.latestCommitData.sha}/linux/vgmstream-audacious.tar.gz`" size="lg" variant="success" class="text-left"><div class="d-inline-flex"><i class="vgs-audacious pr-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i> Audacious plug-in</div></b-btn>
-    </b-button-group>
+    </b-button-group-->
 
   </div>
 </template>
