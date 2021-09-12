86428ef748435eebb814643ab8a0f7a776c94984
Kevin Doncam Demian López Brante <kevin@kddlb.cl>
2021-09-12 23:37:30 +0000

    Removed obituary

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,48 +1,82 @@
 <template>
   <div>
-    <b-navbar type="dark" variant="dark" class="shadow">
-	<b-navbar-nav class="text-center mx-auto">
-		<b-nav-text class="text-white">RIP Near</b-nav-text>
-	</b-navbar-nav>
-    </b-navbar>
     <transition name="fade2">
-      <b-navbar toggleable="md" type="dark" variant="primary" class="shadow" sticky v-if="$route.name !== 'index'">
+      <b-navbar
+        toggleable="md"
+        type="dark"
+        variant="primary"
+        class="shadow"
+        sticky
+        v-if="$route.name !== 'index'"
+      >
         <b-navbar-brand to="/">vgmstream</b-navbar-brand>
-        <b-navbar-toggle target="nav-col"  v-if="$route.name !== 'index'"></b-navbar-toggle>
-          <b-collapse id="nav-col" is-nav  v-if="$route.name !== 'index'">
-            <b-navbar-nav>
-              <b-nav-item to="/about" active-class="active">About</b-nav-item>
-              <b-nav-item to="/downloads" active-class="active">Downloads</b-nav-item>
-              <b-nav-item to="/doc" active-class="active">Documentation</b-nav-item>
-            </b-navbar-nav>
-            <b-navbar-nav class="ml-auto">
-              <github-buttons  />
-            </b-navbar-nav>
-          </b-collapse>
+        <b-navbar-toggle
+          target="nav-col"
+          v-if="$route.name !== 'index'"
+        ></b-navbar-toggle>
+        <b-collapse id="nav-col" is-nav v-if="$route.name !== 'index'">
+          <b-navbar-nav>
+            <b-nav-item to="/about" active-class="active">About</b-nav-item>
+            <b-nav-item to="/downloads" active-class="active"
+              >Downloads</b-nav-item
+            >
+            <b-nav-item to="/doc" active-class="active"
+              >Documentation</b-nav-item
+            >
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <github-buttons />
+          </b-navbar-nav>
+        </b-collapse>
       </b-navbar>
     </transition>
     <b-container class="pt-3 pb-5 vg-push">
-      <Nuxt/>
+      <Nuxt />
     </b-container>
-    <footer class="footer mt-auto py-3 bg-light fixed-bottom" >
+    <footer class="footer mt-auto py-3 bg-light fixed-bottom">
       <b-container>
         <div class="clearfix">
           <div class="float-left">
-            <span class="text-muted">A project of <a href="https://hcs64.com"><i class="vgs-hcs"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></i> Halley's Comet Software</a> and <a href="https://losno.co"><i class="vgs-losnoco"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span></i></a>.</span>
+            <span class="text-muted"
+              >A project of
+              <a href="https://hcs64.com"
+                ><i class="vgs-hcs"
+                  ><span class="path1"></span><span class="path2"></span
+                  ><span class="path3"></span><span class="path4"></span
+                  ><span class="path5"></span><span class="path6"></span
+                  ><span class="path7"></span
+                ></i>
+                Halley's Comet Software</a
+              >
+              and
+              <a href="https://losno.co"
+                ><i class="vgs-losnoco"
+                  ><span class="path1"></span><span class="path2"></span
+                  ><span class="path3"></span><span class="path4"></span
+                  ><span class="path5"></span><span class="path6"></span
+                  ><span class="path7"></span><span class="path8"></span
+                  ><span class="path9"></span><span class="path10"></span
+                  ><span class="path11"></span><span class="path12"></span
+                  ><span class="path13"></span
+                  ><span class="path14"></span></i></a
+              >.</span
+            >
           </div>
-          <div class="float-right">This website is <a href="https://github.com/vgmstream/vgmstream-org">open source</a>.</div>
+          <div class="float-right">
+            This website is
+            <a href="https://github.com/vgmstream/vgmstream-org">open source</a
+            >.
+          </div>
         </div>
       </b-container>
     </footer>
-
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "vgmstreamDefaultLayout"
-}
+  name: "vgmstreamDefaultLayout",
+};
 </script>
 
 <style lang="scss">
@@ -55,9 +89,9 @@ $secondary: #d44d04;
 $success: rgb(30, 105, 29);
 
 //noinspection CssUnknownTarget
-@import 'bootstrap/scss/bootstrap.scss';
+@import "bootstrap/scss/bootstrap.scss";
 //noinspection CssUnknownTarget
-@import 'bootstrap-vue/src/index.scss';
+@import "bootstrap-vue/src/index.scss";
 
 html {
   scroll-behavior: smooth;
@@ -65,7 +99,7 @@ html {
 
 .page-enter-active,
 .page-leave-active {
-  transition: opacity 0.25s
+  transition: opacity 0.25s;
 }
 .page-enter,
 .page-leave-active {
@@ -76,15 +110,17 @@ html {
   transform-origin: top center !important;
 }
 
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 
-.fade2-enter-active, .fade2-leave-active {
-  transition: all .5s;
+.fade2-enter-active,
+.fade2-leave-active {
+  transition: all 0.5s;
 }
 .fade2-enter, .fade2-leave-to /* .fade-leave-active below version 2.1.8 */ {
   transform: scaleY(0);
@@ -94,6 +130,4 @@ html {
 .vgs-hcs {
   font-size: unset !important;
 }
-
-
 </style>
